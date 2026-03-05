c434b8e3374e12b3a9b83a472691cf88f2d7aa9d
Tanvir Hossain <tanvirhossaintufa@gmail.com>
2026-03-05 23:59:52 +0600

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,36 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
-  <aside>
+    <footer className="footer sm:footer-horizontal bg-secondary text-primary p-10">
+  <aside className="text-center mx-auto">
     <Image
       src="https://res.cloudinary.com/dfwigpcer/image/upload/v1772730622/bUILDORA_lOGO_oFFICIAL_Png_deszsa.png"
       alt="ACME Industries Ltd. Logo"
       width={150}
       height={150}
+      className="mx-auto"
     />
-    <p>
-      
+    <p className="mt-[-1.5rem]">
+      <span className="text-lg font-semibold">BUILDORA</span>
       <br />
       Providing reliable tech since 1992
     </p>
   </aside>
-  <nav>
+  <nav className="mx-auto">
     <h6 className="footer-title">Services</h6>
     <a className="link link-hover">Branding</a>
     <a className="link link-hover">Design</a>
     <a className="link link-hover">Marketing</a>
     <a className="link link-hover">Advertisement</a>
   </nav>
-  <nav>
+  <nav className="mx-auto">
     <h6 className="footer-title">Company</h6>
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav>
-  <nav>
+  <nav className="mx-auto">
     <h6 className="footer-title">Legal</h6>
     <a className="link link-hover">Terms of use</a>
     <a className="link link-hover">Privacy policy</a>
